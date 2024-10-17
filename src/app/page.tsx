d79d970a5a86c0d241389be7a7873e44324ec6eb
VasylPopaz <pafos1997@gmail.com>
2024-10-17 22:43:27 +0200

    add posts list to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Divider } from "@nextui-org/react";
 
 import { TopicCreateForm } from "@/components/topics/topic-create-form";
 import { TopicList } from "@/components/topics/topic-list";
+import { PostList } from "@/components/posts/post-list";
+import { fetchTopPosts } from "@/db/queries/posts";
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
         <div className="col-span-3">
           {" "}
           <h1 className="text-xl m-2">Top Posts</h1>
+          <PostList fetchData={fetchTopPosts} />
         </div>
         <div className="border shadow py-3 px-2">
           <TopicCreateForm />
